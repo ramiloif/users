63fd0e5d109d713e0868c00ae70c57d16bf172d1
ramiloif <ramiloif@gmail.com>
2022-03-14 03:28:09 +0200

    finish pupeteerr test

---
+++ README.md
@@ -0,0 +1,13 @@
+# Users
+
+## stracture: 
+/server - nestjs server
+/client - react client
+/e2e - pupeteer e2e test
+
+
+What can be improved: 
+1. DB connection from environment variable nest js props...
+2. spesific error code response in case email alreadu exist for example 409
+3. dont refethch all users on any user create
+4. add some report to pupeteer test and not just exit code
--- e2e/test.js
+++ e2e/test.js
@@ -1,10 +1,12 @@
 const puppeteer = require('puppeteer');
 const { v4: uuidv4 } = require('uuid');
 
+const { MongoClient } = require('mongodb');
+
 (async () => {
   const expectedEmail = `${uuidv4()}@gmail.com`;
 
-  const browser = await puppeteer.launch({headless:false});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('http://localhost:3001', { waitUntil: 'networkidle0' });
   await page.waitForSelector("#create_user_btn")
@@ -17,10 +19,25 @@ const { v4: uuidv4 } = require('uuid');
   await page.type('#description', 'No comment')
   await page.$eval('#submit_form', btn => btn.click());
   await page.waitForSelector("#create_success")
-  
-  // TODO: validate db for expectedEmail
-
-  await page.screenshot({ path: 'test-result.png' });
-
- await browser.close();
+  await page.screenshot({ path: 'test-result.png' });  
+  await browser.close();
+  await validateEmailInDb(expectedEmail)
 })();
+
+
+const validateEmailInDb =  async (expectedEmail) => {
+  const url = 'mongodb://localhost:27017/';
+  const client = new MongoClient(url);
+
+  await client.connect();
+  const db = client.db('lusha')
+  const users = db.collection('users');
+  const user = await users.findOne({ email: expectedEmail })
+  if(!user) {
+    console.log('Error user not really reated in db')
+    process.exit(1);
+  } else {
+    console.log('Success' ,user._id, user.email)
+    process.exit(0);
+  }
+}
